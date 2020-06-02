6fac24cb0c779b599e8154aaf25a0dcac4583df5
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 18:01:19 -0300

    Novo

---
+++ Tabuada/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite a tabuada que deseja consultar: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado;
+            int i;
+            
+            for (i = 0; i <= 10; i++)
+            {
+                resultado = i * numero;
+                Console.WriteLine(i + "x" + numero + "=" + resultado);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
